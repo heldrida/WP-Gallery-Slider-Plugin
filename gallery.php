b941ffacb17efc6b0599937dfda54dd71f034fc0
Helder <heldrida@gmail.com>
2012-02-27 18:09:48 +0000

    Changed Custom Post Type "Gallery" to "Slider Gallery" to be more meaningfull. Created helper functions to save or update Custom Field "Order". Applyed Sortable, jQuery UI function, to trigger the mentioned functions. Theres still work to do, the CPT List, must be ordered by Metafield "Order".

--- gallery.php
+++ gallery.php
@@ -40,12 +40,28 @@ if(! function_exists('cpt_image_box') ){
 if(! function_exists('gal_request_router')){
 	function gal_request_router(){
 		
-		if(isset($_GET['action']) && $_GET['action'] == 'delete_gallery_item' ){
+		if( ! isset($_GET['action']) ){
+			return;
+		};
+
+		switch($_GET['action']){
 			
-			wp_delete_post( $_GET['post'] );
-			wp_redirect( WEBSITE_ADMIN_URL.'edit.php?post_type=cpt-gallery' );
-			exit;
-		}
+			case 'delete_gallery_item':
+
+				wp_delete_post( $_GET['post'] );
+				wp_redirect( WEBSITE_ADMIN_URL.'edit.php?post_type=cpt-gallery' );
+				exit;
+
+			break;
+
+			case 're_order':
+				
+				gal_re_order_slide();
+
+			break;
+
+		};
+
 	}
 };
 if(! function_exists('gal_ajax_request_processor') ){
@@ -102,7 +118,7 @@ if(! function_exists('gal_constructor')){
 		register_post_type( 'cpt-gallery',
 			array(
 				'labels' => array(
-					'menu_name' => 'Gallery',
+					'menu_name' => 'Slides Gallery',
 					'name' => __( 'Galleries' ),
 					'singular_name' => __( 'Gallery' )
 				),
@@ -128,6 +144,9 @@ if(! function_exists('gal_administration_scripts_styles_loader')){
 		wp_register_style('GalleryStyleSheet', GAL_PLUGIN_URL.'/public/css/gallery.css');
 		wp_enqueue_style( 'GalleryStyleSheet');
 
+		wp_enqueue_script('jquery-ui-sortable');
+
+
 	};
 };
 if(! function_exists('gal_cpt_gallery_display_columns')){
@@ -139,7 +158,8 @@ if(! function_exists('gal_cpt_gallery_display_columns')){
 		unset($cols);
 		
 		return array(
-					'ID' => __('Nrº'),
+					'order' => __('Order'),
+					/* 'ID' => __('Nrº'), */
 					'thumb' => __('Preview'),
 					'links_to' => __('Linking to Post'),
 					'post_author' => __('Author'),
@@ -200,6 +220,12 @@ if(! function_exists('gal_cpt_column_action')){
 					
 				break;
 				
+				case 'order':
+					
+					gal_order_handlers();
+
+				break;
+
 				default:
 				
 					echo 'Under development';
@@ -231,11 +257,13 @@ if(! function_exists('gal_meta_fields_nodes')){
 		// Use underscore to prevent showing up in Custom Field Section
 	    $meta = get_post_meta($post->ID,'_cpt_meta_gallery', TRUE);
 		
-		echo '<p><strong>'.(__('Parameters')).':</strong></p>';
+		echo '<p><strong>'.(__('Post title')).':</strong></p>';
 		
 		echo '<input id="post-title-type" type="text" name="post-title-type" value="'.(isset($meta['link-to-post-title']) ? $meta['link-to-post-title']  : null).'">';
 		echo '<input id="link-to-post-title" type="hidden" name="_cpt_meta_gallery[link-to-post-title]" value="">';
 		
+		echo '<p><strong>'.(__('Order')).':</strong></p>';
+		echo '<input type="text" value="'.(isset($meta['cpt-order']) ? $meta['cpt-order']  : null).'" name="cpt-order" />';
 	};	
 };
 if(! function_exists('gal_meta_save')){
@@ -251,16 +279,17 @@ if(! function_exists('gal_meta_save')){
 		global $post;
 		
 		$data = array(
-			'link-to-post-title' => $_POST['post-title-type']
+			'link-to-post-title' => $_POST['post-title-type'],
+			'cpt-order' => $_POST['cpt-order']
 		);
-		
 		update_post_meta($post->ID,'_cpt_meta_gallery', $data );
 		
 		$d = get_post_by_title( $data['link-to-post-title'] );
 		
 		$new_data = array(
 			'link-to-post-title' => $data['link-to-post-title'],
-			'post_id' => $d->ID
+			'post_id' => $d->ID,
+			'cpt-order' => $data['cpt-order']
 		);
 		update_post_meta($post->ID,'_cpt_meta_gallery',$new_data);
 		
@@ -277,3 +306,71 @@ if(! function_exists('get_post_by_title') ){
 	};
 };
 //http://wordpress.org/support/topic/how-to-move-featured-image-box-from-side-to-main-column
+if(! function_exists('gal_cmp')){
+	function gal_cmp($a, $b){
+
+	    if ($a['meta']['cpt-order'] == $b['meta']['cpt-order']) return 0;
+	    return ($a['meta']['cpt-order'] < $b['meta']['cpt-order']) ? -1 : 1;
+	}	
+};
+if(! function_exists('getSlideGalleryData')){
+	function getSlideGalleryData(){
+
+		$args = array(
+			'post_type'       => 'cpt-gallery',
+			'post_status'     => 'publish'
+		); 
+		
+		$posts = get_posts( $args );
+
+		foreach ($posts as $k) {
+			$meta = get_post_meta( $k->ID, '_cpt_meta_gallery', TRUE );
+			$permalink = get_permalink( $m['post_id'] );									
+			$image = wp_get_attachment_url( get_post_thumbnail_id( $k->ID ) );
+			
+			$arr[] = array(
+				'post_title' =>  $k->post_title,
+				'guid' => $k->guid,
+				'meta' => $meta,
+				'permalink' => $permalink,
+				'image' => $image
+			);
+		};
+		usort($arr, 'gal_cmp');
+		
+		return ! empty( $arr ) && is_array($arr) ? $arr : FALSE;
+				
+	};
+};
+if(! function_exists('gal_order_handlers')){
+	function gal_order_handlers(){
+		
+		global $post;
+		
+		$meta = get_post_meta( $post->ID, '_cpt_meta_gallery', TRUE );
+
+		/*
+		echo '<a class="o-mv-up" href="'.(WEBSITE_ADMIN_URL.'edit.php?post_type=cpt-gallery&action=move_up&post_id='.$post->ID).'" title="Move up" style="background: url('.GAL_PLUGIN_URL.'public/images/arrow_up.png) no-repeat 0 0 transparent;">MoveUp</a>';
+		echo '<a class="o-mv-up" href="'.(WEBSITE_ADMIN_URL.'edit.php?post_type=cpt-gallery&action=move_down&post_id='.$post->ID).'" title="Move down" style="background: url('.GAL_PLUGIN_URL.'public/images/arrow_down.png) no-repeat 0 0 transparent;">MoveDown</a>';
+		*/
+		echo '<input class="cpt-o-pos" type="" name="item_order_'.($post->ID).'" value="'.($meta['cpt-order']).'" readonly="readonly">';
+
+	};
+};
+if(! function_exists('gal_re_order_slide')){
+	function gal_re_order_slide(){
+
+		$meta = get_post_meta( $_GET['post_id'], '_cpt_meta_gallery', TRUE );
+
+		$data = array(
+			'link-to-post-title' => $meta['link-to-post-title'],
+			'post_id' => $_GET['post_id'],
+			'cpt-order' => $_GET['order_pos']		
+		);
+		
+		update_post_meta($_GET['post_id'],'_cpt_meta_gallery', $data );		
+		
+		exit;
+
+	};
+};
--- public/js/gallery.js
+++ public/js/gallery.js
@@ -13,6 +13,73 @@
 			});
 			
 		});
+
+		$(function(){
+			$('.cpt-o-pos').on('keyup', function(){
+
+				var post_id = $(this).attr('name').replace(/[^0-9]/ig, '' );
+
+				$.get('', { 'action' : 're_order', 'post_id' : post_id, 'order_pos' : $(this).val() }, function(data) {
+					console.log( data );
+				});
+
+			});
+		});
+
+		$(function() {
+
+			var setOrderNr = function(el){
+					
+				var post_id = $(el).attr('name').replace(/[^0-9]/ig, '' );
+				console.log( 'Post_id: ' + post_id );
+				$.get('', { 'action' : 're_order', 'post_id' : post_id, 'order_pos' : $(el).val() }, function(data) {
+					console.log('Success:');
+					console.log( data );
+				});
+
+			};
+
+			var reOrderItems = function(){
+				console.log( $('.wp-list-table tr') );
+				$('.wp-list-table tbody tr').each(function(k,v){
+
+					var i = k + 1;
+					var curEl = $('.wp-list-table tbody tr').eq(k).find('.cpt-o-pos');
+
+					curEl.val( i );
+					setOrderNr( curEl );
+
+					/*
+			   		var index = $(el.item).index() + 1;
+			   		var curEl = $(el.item).find('.cpt-o-pos');
+
+			   		// set order val
+			   		console.log(index);
+			   		curEl.val( index );
+			   		console.log( curEl.val() );
+					*/
+				});
+
+			};
+
+			$( "#the-list" ).sortable({
+			   update: function(e, el) {
+			   		
+			   		reOrderItems();
+			   		/*
+			   		var index = $(el.item).index() + 1;
+			   		var curEl = $(el.item).find('.cpt-o-pos');
+
+			   		// set order val
+			   		console.log(index);
+			   		curEl.val( index );
+			   		console.log( curEl.val() );
+			   		*/
+			   }
+			});
+			$( "#the-list" ).disableSelection();
+
+		});
 		
 	});
 	
